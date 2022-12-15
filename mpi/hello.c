af9395fc170f079909414ecc8f45082373183d1c
S010MON <leon_debnath@hotmail.co.uk>
2022-12-15 09:26:55 +0100

    Create broadcast

---
+++ mpi/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+* gcc -o hello hello.c
+* ./hello
+* mpiexec -np 3 ./hello
+*/
+int main(int argc, char* argv[]) 
+{
+	printf("Hello World\n");
+	return EXIT_SUCCESS;
+}
